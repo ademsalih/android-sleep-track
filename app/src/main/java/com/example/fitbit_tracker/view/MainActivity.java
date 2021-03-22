d44704e915130ba642ae76079c86294e27ee9a7f
Adem Salih <ademsal.97@icloud.com>
2021-03-22 12:00:18 +0100

    added settings

--- app/src/main/java/com/example/fitbit_tracker/view/MainActivity.java
+++ app/src/main/java/com/example/fitbit_tracker/view/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements ServiceCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().setTitle("Welcome to Nyx");
 
         context = this;
--- app/src/main/java/com/example/fitbit_tracker/view/SettingsActivity.java
+++ app/src/main/java/com/example/fitbit_tracker/view/SettingsActivity.java
@@ -1,8 +1,18 @@
 package com.example.fitbit_tracker.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 import com.example.fitbit_tracker.R;
 
@@ -12,5 +22,58 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportActionBar().setTitle("Settings");
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.settingsFrameLayout, new SettingsFragment()).commit();
     }
+
+    public static PreferenceManager preferenceManager;
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences);
+
+            preferenceManager = getPreferenceManager();
+
+            final SwitchPreference autoTurnOnTVSwitch = (SwitchPreference) getPreferenceManager().findPreference("prefSampling");
+            final EditTextPreference samplingThresholdPreference = (EditTextPreference) getPreferenceManager().findPreference("prefSamplingThreshold");
+
+            if (autoTurnOnTVSwitch != null) {
+                autoTurnOnTVSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        boolean switchState = (boolean) newValue;
+                        FragmentActivity activity = getActivity();
+
+                        if (activity != null) {
+                            SharedPreferences.Editor sp = activity.getSharedPreferences("PREFERENCES", MODE_PRIVATE).edit();
+                            sp.putBoolean("tvAutoOn",switchState).apply();
+                        }
+                        return true;
+                    }
+                });
+            }
+
+            if (samplingThresholdPreference != null) {
+                samplingThresholdPreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                    @Override
+                    public void onBindEditText(@NonNull EditText editText) {
+                        editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+                        editText.setSelection(0,editText.getText().length());
+                    }
+                });
+                samplingThresholdPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        return true;
+                    }
+                });
+            }
+
+        }
+
+    }
+
 }
